1d79df6611d779b77454175ee68da415b9d0e880
neoskurt <49443524+neoskurt@users.noreply.github.com>
2022-07-31 22:26:17 +0200

    Update Pokedex.scala

--- src/main/scala/com/pokemon/Pokedex.scala
+++ src/main/scala/com/pokemon/Pokedex.scala
@@ -21,9 +21,3 @@ object Pokedex{
     }
 }
 }
-
-/*  val scanner = new java.util.Scanner(System.in)
-  Console.println("Choose a Pokemon Name")
-  val line = scanner.nextLine()
-  def main(args: Array[String]): Unit =
-    println(Request.get( line )) */
